767426afc5dea6e2a6ccac2ad634e951d0337921
Tuutej <90647045+Tuutej@users.noreply.github.com>
2023-08-27 13:18:07 +0300

    guess a number lisätty

--- App.js
+++ App.js
@@ -1,10 +1,46 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from "react";
+import { View, Text, Button, Alert, TextInput, StyleSheet } from "react-native";
+import { StatusBar } from "expo-status-bar";
 
-export default function App() {
+export default function GuessNum() {
+  const [guess, setGuess] = useState("");
+  const [answer, setAnswer] = useState("");
+  const [attempts, setAttempts] = useState(0);
+  const [randomizedNum, setRandomizedNum] = useState(randomizeNum());
+
+  function randomizeNum() {
+    return Math.floor(Math.random() * 100) + 1;
+  }
+
+  function handleGuess() {
+    const guessedNum = parseInt(guess);
+
+    if (isNaN(guessedNum)) {
+      setAnswer("Input a valid number!");
+    } else {
+      setAttempts(attempts + 1);
+
+      if (guessedNum < randomizedNum) {
+        setAnswer(`Your guess ${guessedNum} is too low!`);
+      } else if (guessedNum > randomizedNum) {
+        setAnswer(`Your guess ${guessedNum} is too high!`);
+      } else {
+        setAnswer(
+          `Congratulations! You guessed the correct number ${randomizedNum} in ${attempts} guesses.`
+        );
+      }
+    }
+  }
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>Guess a number between 1-100!</Text>
+      <View style={{ marginTop: 20 }} />
+      <TextInput value={guess} onChangeText={setGuess} keyboardType="numeric" />
+      <View style={{ marginTop: 20 }} />
+      <Button title="Make a guess" onPress={handleGuess} />
+      <View style={{ marginTop: 20 }} />
+      <Text>{answer}</Text>
       <StatusBar style="auto" />
     </View>
   );
@@ -13,8 +49,8 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
